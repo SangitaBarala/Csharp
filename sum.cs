2d32128ac1ccbe17d83bbeecec1d68006936770f
sangitaBarala <sangitakhatkar.0425@gmail.com>
2020-11-12 19:02:59 -0500

    sum

---
+++ sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class sum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(3, 5));
+            Console.WriteLine(Sum(3, 3));
+            Console.ReadLine();
+        }
+        public static int Sum(int a, int b)
+        {
+            if(a==b)
+            {
+                return (a + b) * 3;
+            }
+            else
+            {
+                return a + b;
+            }
+                
+        }
+
+    }
+}
+
